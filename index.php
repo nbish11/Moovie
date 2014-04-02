22adaf5d5e375d77dfc9e911238a591d105ecdac
Nathan <nbish11@hotmail.com>
2014-04-02 23:49:54 +1000

    General housekeeping

--- index.php
+++ index.php
@@ -5,16 +5,20 @@
         <meta charset="utf-8">
         <title>Moovie</title>
         
-        <link rel="stylesheet" href="/css/moovie.css">
+        <link rel="stylesheet" href="//localhost/Moovie/src/css/Moovie-0.0.1alpha.css">
     </head>
     <body>
-        <video id="myvideo" autobuffer="" poster="http://colinaarts.com/assets/avatar.png" src="http://colinaarts.com/assets/avatar.ogv" controls>
-            <p>Your browser does not support the HTML 5<code style="display: none;">video</code><code class="standardLighter ">video</code>element.</p>
-        </video>
+    <div class="moovie" style="width: 600px; height: 250px;">
+        <div class="wrapper">
+            <video id="myvideo" autobuffer="" poster="http://colinaarts.com/assets/avatar.png" src="http://colinaarts.com/assets/avatar.ogv" controls>
+                <p>Your browser does not support the HTML 5<code style="display: none;">video</code><code class="standardLighter ">video</code>element.</p>
+            </video>
+        </div>
+    </div>
         
-        <script src="//ajax.googleapis.com/ajax/libs/mootools/1.2.4/mootools-yui-compressed.js"></script>
-        <!-- <script src="//localhost/Moovie/mootools-more-1.4.0.1.js"></script> -->
-        <script src="//localhost/Moovie/Moovie-0.0.1alpha.js"></script>
+        <script src="//ajax.googleapis.com/ajax/libs/mootools/1.4.5/mootools-yui-compressed.js"></script>
+        <script src="//localhost/Moovie/mootools-more-1.4.0.1.js"></script>
+        <script src="//localhost/Moovie/src/js/vendor/Moovie/Moovie-0.0.1alpha.js"></script>
         
         <script>
             var video = {
