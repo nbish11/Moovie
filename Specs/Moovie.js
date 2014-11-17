5500b2e59301f6d2b31a221a3ade965bb31132c0
Nathan Bishop <nbish11@hotmail.com>
2014-11-17 16:46:28 +1000

    made test more readable

--- Specs/Moovie.js
+++ Specs/Moovie.js
@@ -5,7 +5,7 @@ describe('Moovie Test Suite', function () {
     
     it('expect Moovie() to be defined and a function', function () {
         expect(Moovie).toBeDefined();
-        expect(typeof Moovie === 'function').toBe(true);
+        expect(typeof Moovie).toBe('function');
     });
     
 });
