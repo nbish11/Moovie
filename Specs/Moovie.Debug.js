47fdd121167b8dc1872029b09b2994a9df320a78
Nathan Bishop <nbish11@hotmail.com>
2015-05-05 05:53:03 +1000

    Released "Debug" class as a Moovie module

---
+++ Specs/Moovie.Debug.js
@@ -0,0 +1,10 @@
+/*global Moovie */
+
+describe('Moovie.Debug', function () {
+    'use strict';
+    
+    it('is a defined module', function () {
+        expect(Moovie.Debug).toBeA('class');
+    });
+    
+});
