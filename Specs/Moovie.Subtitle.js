781da96f615b7cf5a0e3fbd5a8267d2865616c4b
Nathan Bishop <nbish11@hotmail.com>
2015-05-07 03:19:44 +1000

    Created "Subtitle" module where all subtitle parsing, loading and displaying will eventually be moved to.

---
+++ Specs/Moovie.Subtitle.js
@@ -0,0 +1,8 @@
+/* global Moovie */
+describe('Moovie.Subtitle', function () {
+    'use strict';
+    
+    it('is a module', function () {
+        expect(Moovie.Subtitle).toBeA('object');
+    });
+});
